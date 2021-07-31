0156c301b5a2b48bf51bf68f1dc84accb01fa503
Steve <steve.tyrell@gmail.com>
2021-07-31 21:52:44 +0100

    first commit

---
+++ lib/questions.dart
@@ -0,0 +1,13 @@
+class Question {
+  late String questionText;
+  late bool answer = false;
+
+  // Question(String q, bool a) {
+  //   questionText = q;
+  //   answer = a;
+  // }
+
+  Question(this.questionText, this.answer) {}
+  // removed {} so that don't need to specifically state q: a:
+
+}
